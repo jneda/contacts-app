df32f1ff17292178ca703a1d04257005b25528b9
jneda <julien.dane@gmail.com>
2023-03-02 15:39:41 +0100

    POST contacts API route

--- app.js
+++ app.js
@@ -17,6 +17,17 @@ app.get("/api/contacts", async (req, res) => {
   }
 });
 
+app.post("/api/contacts", async (req, res) => {
+  try {
+    const { first: firstName, last: lastName, email } = req.body;
+    const contact = await db.Contact.create({ firstName, lastName, email });
+    return res.send(contact);
+  } catch (err) {
+    console.error("An error occurred posting a contact:", JSON.stringify(err));
+    return res.status(500).send(JSON.stringify(err));
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000.");
 });
