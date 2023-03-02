441cf05cc9cc0deaab01c5dcbf75c868baa39a02
jneda <julien.dane@gmail.com>
2023-03-02 15:09:34 +0100

    GET contacts API route + contacts display in index view

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const db = require("./models");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/public`));
+
+app.get("/api/contacts", async (req, res) => {
+  try {
+    const contacts = await db.Contact.findAll({ raw: true });
+    return res.send(contacts);
+  } catch (err) {
+    console.error("An error occurred querying contacts:", JSON.stringify(err));
+    return res.status(500).send(JSON.stringify(err));
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000.");
---
+++ public/scripts/index.js
@@ -0,0 +1,21 @@
+const contactsList = document.querySelector("#contacts-list");
+window.addEventListener("load", loadContacts);
+
+async function loadContacts() {
+  console.log("Loading contacts...");
+  try {
+    const response = await fetch("http://localhost:3000/api/contacts");
+    const contacts = await response.json();
+    const contactsHTML = contacts.reduce((html, contact) => {
+      return (
+        html +
+        `<li>
+          ${contact.firstName} ${contact.lastName}: ${contact.email}
+        </li>`
+      );
+    }, "");
+    contactsList.innerHTML = contactsHTML;
+  } catch (err) {
+    console.error(err.message);
+  }
+}
